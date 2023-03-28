d5e6f8067a9ed40397df7423bf1f9a11222d6e8b
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-27 22:18:40 -0600

    Add the avatar and the user name in the header. As well as style it. Add the input text to send post request to the server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
