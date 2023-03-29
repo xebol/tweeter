950b3f781b9e66d144179c6d1aa310047d28d6c2
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-28 18:31:11 -0600

    Add character counter to count if the tweet statement has surpassed the 140 character limit. The count then turns red to indicate that the limit was reached

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#tweet-text').on('input', function(e) {
+    const character = e.target.value.length;
+    const remainingCharacter = 140 - character;
+    const counter = $('.counter');
+    counter.text(remainingCharacter);
+    if (remainingCharacter < 0) {
+      counter.addClass('red-text');
+    } else {
+      counter.removeClass('red-text');
+    }
+  });
+});
